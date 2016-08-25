2ba7b4854626b145160e6437d364ddaef7ba8329
Janina <janina.bratu@gmail.com>
2016-08-25 22:51:57 +0300

    GreetingDay in progress

--- TrainningNet/OperatorAction.cs
+++ TrainningNet/OperatorAction.cs
@@ -8,88 +8,83 @@ namespace TrainningNet
 {
     public class OperatorAction
     {
+
+
         
 
-        public static string GetPeriodOfDay(DateTime actualTime)
+        public static void GreetByPeriodDay(DateTime actualTime)
         {
-            return null;
+
+            var userInput = Console.ReadLine();
+
+            while ((userInput != null))
+            {
+                //if (morningMinTime < Convert.ToInt32(actualTime) < Convert.ToInt32(morningMaxTime) )
+                //{
+
+                //}
+            }
         }
 
-        /*
-         * "Good Morning"
-         * "Good Afternoon"
-         * "Good Evening"
-         * "Good Night"
-         * 
-         */
-
-
-        public static StringBuilder SetMorningPeriod()
+        public List<DateTime> GetListOfPeriodsDay()
         {
-            string startHour = "03";
-            string endHour = "11";
-            string startMinute = "00";
-            string endMinute = "59";
-            string points = ":";
-            string space = " ";
-            string anteMeridiem = "AM";
+            List<DateTime> listOfDates = new List<DateTime>();
 
-            StringBuilder morningPeriodBuilder = new StringBuilder();
-            morningPeriodBuilder.Append(startHour).Append(points).Append(endHour).Append(points)
-                         .Append(startMinute).Append(points).Append(endMinute).Append(space).Append(anteMeridiem);
+            //"Good Morning"
+            DateTime morningMinTime = new DateTime();
+            DateTime morningMaxTime = new DateTime();
 
-            return morningPeriodBuilder;
+            TimeSpan startMorningTimeStamps = new TimeSpan(03, 00, 0);
+            morningMinTime = morningMinTime.Date + startMorningTimeStamps;
+
+            TimeSpan endMorningTimeStamps = new TimeSpan(11, 00, 0);
+            morningMaxTime = morningMaxTime + endMorningTimeStamps;
+
+            //"Good Afternoon"
+            DateTime afternoonMinTime = new DateTime();
+            DateTime afternoonMaxTime = new DateTime();
+
+            TimeSpan startAfternoonTimeStamps = new TimeSpan(12, 00, 0);
+            afternoonMinTime = afternoonMinTime.Date + startAfternoonTimeStamps;
+
+            TimeSpan endAfternoonTimeStamps = new TimeSpan(13, 00, 0);
+            afternoonMaxTime = afternoonMaxTime + endAfternoonTimeStamps;
+
+            //"Good Evening"
+            DateTime eveningMinTime = new DateTime();
+            DateTime eveningMaxTime = new DateTime();
+
+            TimeSpan startEveningTimeStamps = new TimeSpan(14, 00, 1);
+            eveningMinTime = eveningMinTime.Date + startEveningTimeStamps;
+
+            TimeSpan endEveningTimeStamps = new TimeSpan(17, 00, 1);
+            eveningMaxTime = eveningMaxTime + endEveningTimeStamps;
+
+            //"Good Night"
+            DateTime nightMinTime = new DateTime();
+            DateTime nightMaxTime = new DateTime();
+
+            TimeSpan startNightTimeStamps = new TimeSpan(18, 00, 1);
+            nightMinTime = nightMinTime.Date + startNightTimeStamps;
+
+            TimeSpan endNightTimeStamps = new TimeSpan(02, 00, 1);
+            nightMaxTime = nightMaxTime + endNightTimeStamps;
+
+            listOfDates.Add(morningMinTime);
+            listOfDates.Add(morningMaxTime);
+
+            listOfDates.Add(afternoonMinTime);
+            listOfDates.Add(afternoonMaxTime);
+
+            listOfDates.Add(eveningMinTime);
+            listOfDates.Add(eveningMaxTime);
+
+            listOfDates.Add(nightMinTime);
+            listOfDates.Add(nightMaxTime);
+
+            return listOfDates;
         }
 
-        public static StringBuilder SetAfternoonPeriod()
-        {
-            string startHour = "12";
-            string endHour = "13";
-            string startMinute = "00";
-            string endMinute = "59";
-            string points = ":";
-            string space = " ";
-            string anteMeridiem = "AM";
 
-            StringBuilder periodBuilder = new StringBuilder();
-            periodBuilder.Append(startHour).Append(points).Append(endHour).Append(points)
-                         .Append(startMinute).Append(points).Append(endMinute).Append(space).Append(anteMeridiem);
-
-            return periodBuilder;
-        }
-
-        public static StringBuilder SetEveningPeriod()
-        {
-            string startHour = "14";
-            string endHour = "17";
-            string startMinute = "00";
-            string endMinute = "59";
-            string points = ":";
-            string space = " ";
-            string postMeridiem = "PM";
-
-            StringBuilder periodBuilder = new StringBuilder();
-            periodBuilder.Append(startHour).Append(points).Append(endHour).Append(points)
-                         .Append(startMinute).Append(points).Append(endMinute).Append(space).Append(postMeridiem);
-
-            return periodBuilder;
-        }
-
-        public static StringBuilder SetNightPeriod()
-        {
-            string startHour = "18";
-            string endHour = "02";
-            string startMinute = "00";
-            string endMinute = "59";
-            string points = ":";
-            string space = " ";
-            string postMeridiem = "PM";
-
-            StringBuilder periodBuilder = new StringBuilder();
-            periodBuilder.Append(startHour).Append(points).Append(endHour).Append(points)
-                         .Append(startMinute).Append(points).Append(endMinute).Append(space).Append(postMeridiem);
-
-            return periodBuilder;
-        }
     }
 }
